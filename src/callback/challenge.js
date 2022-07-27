c4a4b3e577451ca923eb7e5a0e2b818c44d6aea3
Daniel Jaimes <djaimes.dev@gmail.com>
2022-07-26 21:36:28 -0500

    un script y el uso de callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -35,11 +35,11 @@ function fetchData(urlApi, callback) {
 
 fetchData(`${API}/products`, function(error1, data1){
     if(error1) return console.error(error1)
-    fetchData(`${API}/products/${data1[1].id}`, function(error2,data2){
+    fetchData(`${API}/products/${data1[2].id}`, function(error2,data2){
         if(error2) return console.error(error2)
         fetchData(`${API}/categories/${data2?.category?.id}`,function(error3,data3){
             if(error3) return console.error(error3)
-            console.log(data1[1])
+            console.log(data1[2])
             console.log(data2.title)
             console.log(data3.name)
         })
